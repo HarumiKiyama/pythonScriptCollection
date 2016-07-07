c2a6d38903939dd703c6b1e438e2bfd2a8e4c5d0
Vergilius <lucius0720@hotmail.com>
2016-07-07 23:06:40 +0800

    add script skeleton

---
+++ email_kindle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Using python and my hotmail account to send mobi, aw or pdf file to my kindle"""
+def get_file():
+    pass
+def send2kindle():
+    pass
+def crucial_information(email_account,kindle_email):
+    """
+    :return {} 
+    """
+    pass
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
