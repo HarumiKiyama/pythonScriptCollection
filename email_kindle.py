45f0cf4b6cb2c9c06e11641b9a7e857da1fb7188
Vergilius <lucius0720@hotmail.com>
2016-07-07 23:31:46 +0800

    add personal information

--- email_kindle.py
+++ email_kindle.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Using python and my hotmail account to send mobi, aw or pdf file to my kindle"""
-def get_file():
-    pass
+import smtplib
+from email.mime.text import MIMEText
+
+
+def email_content(file_path):
+    with open(file_path,'rb') as fp:
+        fp=get_file('path')
+        msg=MIMEText(fp.read())
+        personal_information=('lucius0720@hotmail.com','*****',
+        'lucius0720@kindle.cn')
+        msg['Subject']='The contents of my book'
+        msg['From']=personal_information[0]
+        msg['To']=personal_information[2]
+
+
 def send2kindle():
     pass
-def crucial_information(email_account,kindle_email):
-    """
-    :return {} 
-    """
-    pass
 def main():
-    pass
+    my_information=crucial_information('lucius0720@hotmail.com','*****',
+                                       'lucius0720@kindle.cn')
+
 
 if __name__ == '__main__':
     main()
